41e6cc5c8f7548875bf324c66f02dfa333d87e4e
nitin24july <nitindarade786@gmail.com>
2023-05-29 03:04:43 +0530

    getter and setter methods added

--- QuizApplication/src/com/demo/StudentReg.java
+++ QuizApplication/src/com/demo/StudentReg.java
@@ -2,8 +2,106 @@ package com.demo;
 
 public class StudentReg {
 	
-	public static void main(String[] args) {
-		System.out.println("This is Student Registration");
+	
+	String firstName;
+	String lastName;
+	String userName;
+	String password;
+	String city;
+	long contact;
+	
+	
+	
+	public StudentReg() {
+		super();
 	}
+
+
+
+	public StudentReg(String firstName, String lastName, String userName, String password, String city, long contact) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.city = city;
+		this.contact = contact;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+
+	public long getContact() {
+		return contact;
+	}
+
+
+
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	
+	
+	
+	
 
 }
